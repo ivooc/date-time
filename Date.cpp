7cbc790376907b60d8dc7d9075e4269bf286464d
Ivo Capanema <ivoliveirabh@hotmail.com>
2019-11-16 00:08:06 -0300

    Removed using namespace std
    added .gitignore

--- Date.cpp
+++ Date.cpp
@@ -2,12 +2,12 @@
 
 // expressao regular para avaliar validade de uma data no formato dd/mm/yyyy
 // ver https://regexr.com/3b9td (adaptado)
-const regex Date::re("(^(((0[1-9]|1[0-9]|2[0-8])[/](0[1-9]|1[012]))|((29|30|31)[/](0[13578]|1[02]))|((29|30)[/](0[4,6,9]|11)))[/](19|[2-9][0-9])[[:digit:]][[:digit:]]$)|(^29[/]02[/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)");
+const std::regex Date::re("(^(((0[1-9]|1[0-9]|2[0-8])[/](0[1-9]|1[012]))|((29|30|31)[/](0[13578]|1[02]))|((29|30)[/](0[4,6,9]|11)))[/](19|[2-9][0-9])[[:digit:]][[:digit:]]$)|(^29[/]02[/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)");
 
 //http://www.cplusplus.com/articles/2wA0RXSz/
-const vector<string> Date::explode(const string& s, const char& c) {
-	string buff{ "" };
-	vector<string> v;
+const std::vector<std::string> Date::explode(const std::string& s, const char& c) {
+	std::string buff{ "" };
+	std::vector<std::string> v;
 
 	for (auto n : s)
 	{
@@ -18,8 +18,8 @@ const vector<string> Date::explode(const string& s, const char& c) {
 
 	return v;
 }
-time_t Date::stringToTime(const string& s) {
-	vector<string> tokens{ explode(s, '/') };
+time_t Date::stringToTime(const std::string& s) {
+	std::vector<std::string> tokens{ explode(s, '/') };
 	int dia, mes, ano;
 	dia = stoi(tokens[0]);
 	mes = stoi(tokens[1]);
@@ -40,7 +40,7 @@ Date::Date() {
 	if (numCharConv > 0) dataString = aux; // se 0, o numero de caracteres foi excedido
 	else throw "a conversao da data para string falhou";
 }
-Date::Date(const string& ds) {
+Date::Date(const std::string& ds) {
 	if (regex_match(ds, Date::re))
 		dataString = ds;
 	else throw "data invalida";
@@ -50,10 +50,10 @@ Date& Date::Hoje() {
 	dataString = aux.dataString;
 	return *this;
 }
-const string Date::pegarData() const {
+const std::string Date::pegarData() const {
 	return dataString;
 }
-void Date::setarData(const string& d) {
+void Date::setarData(const std::string& d) {
 	if (regex_match(d, Date::re)) {
 		dataString = d;
 	}
@@ -77,20 +77,20 @@ bool Date::operator< (const Date& d) const {
 bool Date::operator== (const Date& d) const {
 	return (dataString == d.dataString);
 }
-bool Date::operator== (const string& s) const {
+bool Date::operator== (const std::string& s) const {
 	return (dataString == s);
 }
 bool Date::operator!= (const Date& d) const {
 	return (dataString != d.dataString);
 }
-bool Date::operator!= (const string& s) const {
+bool Date::operator!= (const std::string& s) const {
 	return (dataString != s);
 }
-ostream& operator<< (ostream& out, const Date& d) {
+std::ostream& operator<< (std::ostream& out, const Date& d) {
 	out << d.dataString;
 	return out;
 }
-istream& operator>> (istream& in, Date& d) {
+std::istream& operator>> (std::istream& in, Date& d) {
 	char s[11];
 	in.getline(s, 11);
 	if (regex_match(s, Date::re)) {
--- Date.h
+++ Date.h
@@ -6,28 +6,26 @@
 #include <regex>
 #include <vector>
 
-using namespace std;
-
 class Date {
-	string dataString;
-	static const regex re;
-	static const vector<string> explode(const string&, const char&);
-	static time_t stringToTime(const string&);
+	std::string dataString;
+	static const std::regex re;
+	static const std::vector<std::string> explode(const std::string&, const char&);
+	static time_t stringToTime(const std::string&);
 public:
 	Date(); // constroi um Date com a hora de agora
-	Date(const string&); // constroi um Date com a hora passada por string em formato dd/mm/yyyy
+	Date(const std::string&); // constroi um Date com a hora passada por string em formato dd/mm/yyyy
 	Date& Hoje(); // seta a data para o dia de hoje
-	const string pegarData() const; // retorna string dd/mm/yyyy
-	void setarData(const string&); // seta a data do objeto
+	const std::string pegarData() const; // retorna string dd/mm/yyyy
+	void setarData(const std::string&); // seta a data do objeto
 	void adicionarDias(const int&); // adiciona dias
 	int difDias (const Date&) const; // retorna dias de diferenca entre o parametro e o objeto (parametro - objeto)
 	bool operator< (const Date&) const;
 	bool operator== (const Date&) const;
-	bool operator== (const string&) const;
+	bool operator== (const std::string&) const;
 	bool operator!= (const Date&) const;
-	bool operator!= (const string&) const;
-	friend ostream& operator<< (ostream&, const Date&);
-	friend istream& operator>> (istream&, Date&);
+	bool operator!= (const std::string&) const;
+	friend std::ostream& operator<< (std::ostream&, const Date&);
+	friend std::istream& operator>> (std::istream&, Date&);
 	~Date();
 };
 
